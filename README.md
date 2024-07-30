8b63202c07fa3b7fa430186c42e4b86d8aeb56cf
bradenkwebb <bkwebb23@gmail.com>
2024-07-30 00:25:54 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Terrain Renderer
+
+## Set-up
+To build and run this program on Windows, the following tools should be available:
+
+```
+gcc --version
+g++ --version
+gdb --version
+```
+In order to use the C/C++ debugger on VS Code, I also make sure to open the directory by running the
+Developer Command Prompt for VS, navigating to the directory, and running `code .` from within the directory.
+
+
---
+++ helloworld.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <windows.h>
+#include "GLFW/glfw3.h"
+#include <opengl32.lib>
+
+int main() {
+    // if (!glfwInit()) {
+    //     std::cerr << "Failed to initialize GLFW" << std::endl;
+    //     return -1;
+    // }
+    int numSecs = 5;
+    // Sleep(numSecs);
+
+    std::cout << "Hello World!" << std::endl;
+    std::cout << __cplusplus << std::endl;
+    // std::cout << std::format("Sleeping for {} seconds", str(numSecs)) << std::endl;
+    return 0;
+}
