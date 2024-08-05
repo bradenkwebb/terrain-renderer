a008760def1504d437a5acfe2a24574da786969a
bradenkwebb <bkwebb23@gmail.com>
2024-08-05 09:47:06 -0400

    remove unnecessary this-> pointers in class defs

--- src/shader.cpp
+++ src/shader.cpp
@@ -40,7 +40,7 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath) {
     glCompileShader(fragment);
     checkCompileErrors(fragment, "FRAGMENT");       
 
-    this->ID = glCreateProgram();
+    ID = glCreateProgram();
     glAttachShader(ID, vertex);
     glAttachShader(ID, fragment);
     glLinkProgram(ID);
@@ -49,19 +49,19 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath) {
 };
 
 void Shader::use() {
-    glUseProgram(this->ID);
+    glUseProgram(ID);
 }
 
 void Shader::setBool(const std::string &name, bool value) const {
-    glUniform1i(glGetUniformLocation(this->ID, name.c_str()), (int)value);
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
 }
 
 void Shader::setInt(const std::string &name, int value) const {
-    glUniform1i(glGetUniformLocation(this->ID, name.c_str()), value);
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
 }
 
 void Shader::setFloat(const std::string &name, float value) const {
-    glUniform1i(glGetUniformLocation(this->ID, name.c_str()), value);
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
 }
 
 void Shader::checkCompileErrors(unsigned int shader, std::string type) {
--- src/terrain_renderer.cpp
+++ src/terrain_renderer.cpp
@@ -10,17 +10,17 @@
 #include "shader.hpp"
 
 TerrainRenderer::TerrainRenderer() {
-    this->hasPath = false;
+    hasPath = false;
 }
 
 TerrainRenderer::TerrainRenderer(std::string heightMapPath) {
     this->heightMapPath = heightMapPath;
-    this->hasPath = true;
+    hasPath = true;
 };
 
 TerrainRenderer::~TerrainRenderer() {
-    if (this->heightMap) {
-        delete[] this->heightMap;
+    if (heightMap) {
+        delete[] heightMap;
     }
 }
 
@@ -41,65 +41,65 @@ void TerrainRenderer::loadHeightMap(std::string const& heightMapPath) {
     std::cout << "Height map loaded successfully! Height: " << height << " Width: " << width << std::endl;
 
     this->heightMapPath = heightMapPath;
-    this->hasPath = true;
-    this->terHeight = height;
-    this->terWidth = width;
     this->heightMap = heightMap;
+    hasPath = true;
+    terHeight = height;
+    terWidth = width;
     return;
 };
 
 void TerrainRenderer::loadHeightMap() {
-    if (!this->hasPath) {
+    if (!hasPath) {
         throw "The heightMapPath must be provided either at object initilization, or as an argument.";
     } else {
-        this->loadHeightMap(this->heightMapPath);
+        loadHeightMap(heightMapPath);
     }
     return;
 }
 
 void TerrainRenderer::createMesh() {
-    for (unsigned int y = 0; y < this->terHeight; y++) {
-        for (unsigned int x = 0; x < this->terWidth; x++) {
-            this->terMeshVertices.push_back((float)x / (this->terWidth - 1)); // x-coordinate
-            this->terMeshVertices.push_back(heightMap[this->terWidth * y + x]); // height, or y-coordinate
-            this->terMeshVertices.push_back((float)y / (this->terHeight - 1));
+    for (unsigned int y = 0; y < terHeight; y++) {
+        for (unsigned int x = 0; x < terWidth; x++) {
+            terMeshVertices.push_back((float)x / (terWidth - 1)); // x-coordinate
+            terMeshVertices.push_back(heightMap[terWidth * y + x]); // height, or y-coordinate
+            terMeshVertices.push_back((float)y / (terHeight - 1));
         }
     }
-    std::cout << "There are " << this->terMeshVertices.size() << " vertices." << std::endl;
+    std::cout << "There are " << terMeshVertices.size() << " vertices." << std::endl;
 
-    for (unsigned int y = 0; y < this->terHeight - 1; y++) {
-        for (unsigned int x = 0; x < this->terWidth - 1; x++) {
+    for (unsigned int y = 0; y < terHeight - 1; y++) {
+        for (unsigned int x = 0; x < terWidth - 1; x++) {
             // Indices of the 4 vertices defining a grid cell
             // We should have two triangles for each square cell
-            unsigned int topLeft = y * this->terWidth + x;
-            unsigned int topRight = y * this->terWidth + x + 1;
-            unsigned int bottomLeft = (y + 1) * this->terWidth + x;
-            unsigned int bottomRight = (y + 1) * this->terWidth + x + 1;
+            unsigned int topLeft = y * terWidth + x;
+            unsigned int topRight = y * terWidth + x + 1;
+            unsigned int bottomLeft = (y + 1) * terWidth + x;
+            unsigned int bottomRight = (y + 1) * terWidth + x + 1;
 
             // Triangle 1 vertices
-            this->terMeshIndices.push_back(topLeft);
-            this->terMeshIndices.push_back(bottomLeft);
-            this->terMeshIndices.push_back(topRight);
+            terMeshIndices.push_back(topLeft);
+            terMeshIndices.push_back(bottomLeft);
+            terMeshIndices.push_back(topRight);
 
             // Triangle 2 vertices
-            this->terMeshIndices.push_back(topRight);
-            this->terMeshIndices.push_back(bottomLeft);
-            this->terMeshIndices.push_back(bottomRight);
+            terMeshIndices.push_back(topRight);
+            terMeshIndices.push_back(bottomLeft);
+            terMeshIndices.push_back(bottomRight);
         }
     }
-    std::cout << "There are " << this->terMeshIndices.size() << " triangle indices." << std::endl;
+    std::cout << "There are " << terMeshIndices.size() << " triangle indices." << std::endl;
 
-    glGenVertexArrays(1, &this->VAO);
-    glGenBuffers(1, &this->VBO);
-    glGenBuffers(1, &this->EBO);
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
-    glBindVertexArray(this->VAO);
+    glBindVertexArray(VAO);
 
-    glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
-    glBufferData(GL_ARRAY_BUFFER, this->terMeshVertices.size() * sizeof(float), &this->terMeshVertices[0], GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, terMeshVertices.size() * sizeof(float), &terMeshVertices[0], GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, this->terMeshIndices.size() * sizeof(unsigned int), &this->terMeshIndices[0], GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, terMeshIndices.size() * sizeof(unsigned int), &terMeshIndices[0], GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -109,7 +109,7 @@ void TerrainRenderer::createMesh() {
 }
 
 void TerrainRenderer::render() {
-    glUseProgram(this->shaderProgram);
+    glUseProgram(shaderProgram);
     // Set up model, view and projection matrices
     glm::mat4 model = glm::mat4(1.0f);
     glm::mat4 view = glm::lookAt(
@@ -125,9 +125,9 @@ void TerrainRenderer::render() {
     );
 
     // Get location of the uniform variables in the shader program
-    int modelLoc = glGetUniformLocation(this->shaderProgram, "model");
-    int viewLoc = glGetUniformLocation(this->shaderProgram, "view");
-    int projectionLoc = glGetUniformLocation(this->shaderProgram, "projection");
+    int modelLoc = glGetUniformLocation(shaderProgram, "model");
+    int viewLoc = glGetUniformLocation(shaderProgram, "view");
+    int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
 
     // Set the uniform variables in the shader program
     glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
@@ -135,7 +135,7 @@ void TerrainRenderer::render() {
     glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
     // Bind the VAO and draw the elements
-    glBindVertexArray(this->VAO);
-    glDrawElements(GL_TRIANGLES, this->terMeshIndices.size(), GL_UNSIGNED_INT, 0);
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, terMeshIndices.size(), GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
 }
