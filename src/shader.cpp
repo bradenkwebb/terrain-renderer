65a123d738557196d228daf25e0ff0ee9f0b0a88
bradenkwebb <bkwebb23@gmail.com>
2024-08-07 00:44:35 -0400

    add new function for setting matrix uniforms

--- src/shader.cpp
+++ src/shader.cpp
@@ -1,3 +1,5 @@
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include "shader.hpp"
 
 Shader::Shader(const char* vertexPath, const char* fragmentPath) {
@@ -50,19 +52,23 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath) {
 
 void Shader::use() {
     glUseProgram(ID);
-}
+};
 
 void Shader::setBool(const std::string &name, bool value) const {
     glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
-}
+};
 
 void Shader::setInt(const std::string &name, int value) const {
     glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
-}
+};
 
 void Shader::setFloat(const std::string &name, float value) const {
     glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
-}
+};
+
+void Shader::setMat4(const std::string &name, glm::mat4 mat) const {
+    glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(mat));
+};
 
 void Shader::checkCompileErrors(unsigned int shader, std::string type) {
     int success;
@@ -81,4 +87,4 @@ void Shader::checkCompileErrors(unsigned int shader, std::string type) {
         }
     }
 
-}
+};
