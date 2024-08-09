f302a0983b0f919daaef9ee94dd651b6cd4a6a5b
bradenkwebb <bkwebb23@gmail.com>
2024-08-09 00:42:02 -0400

    create initial mesh class

---
+++ include/mesh.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <glm/glm.hpp>
+#include "shader.hpp"
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+struct Texture {
+    unsigned int id;
+    std::string type;
+};
+
+class Mesh {
+private:
+    unsigned int VAO, VBO, EBO;
+    void setupMesh();    
+public:
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture> textures;
+
+    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+    void Draw(Shader &shader);
+
+};
